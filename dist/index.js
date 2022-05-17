32678a4a1310c1710b5dab88763abb2e98875981
ldu1020 <ldu1020@gmail.com>
2022-05-17 23:05:18 +0900

    feat: basic

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("hi2");
+// Pure Function
+const duplicate = (str, num) => {
+    return num < 1 ? "" : str + duplicate(str, num - 1);
+};
+// Higher Order Function
+const withLog = (fn) => (...arg) => {
+    console.log(fn.name);
+    return fn(...arg);
+};
+//Currying
+const add = (a) => (b) => {
+    return a + b;
+};
+// Composition
+const range = (from, to) => from > to ? [] : [from, ...range(from + 1, to)];
+const multiply = (...args) => args.reduce((acc, cur) => acc * cur);
+const factorial = (num) => multiply(...range(1, num));
--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-console.log("hi2");
+type Fn = (...args: any[]) => any;
+
+// Pure Function
+const duplicate = (str: string, num: number): string => {
+  return num < 1 ? "" : str + duplicate(str, num - 1);
+};
+
+// Higher Order Function
+const withLog =
+  <T extends Fn>(fn: T) =>
+  (...arg: Parameters<typeof fn>): ReturnType<typeof fn> => {
+    console.log(fn.name);
+    return fn(...arg);
+  };
+
+//Currying
+const add = (a: number) => (b: number) => {
+  return a + b;
+};
+
+// Composition
+const range = (from: number, to: number): number[] =>
+  from > to ? [] : [from, ...range(from + 1, to)];
+
+const multiply = (...args: number[]) => args.reduce((acc, cur) => acc * cur);
+
+const factorial = (num: number) => multiply(...range(1, num));
